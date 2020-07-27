c8a8f1594501ec2f7d695c636dab5d2adfc097b0
Ewerton Luna <ewerton.al@gmail.com>
2020-07-27 18:56:36 -0300

    Remove auth for GET /courses

---
+++ client/src/components/Courses.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import baseApiURL from '../config';
+
+class Courses extends React.Component {
+  state = {
+    courses: [],
+  };
+
+  componentDidMount() {
+    
+  }
+
+
+
+  render(){
+    
+  }
+}
